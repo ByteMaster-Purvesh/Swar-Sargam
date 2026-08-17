902f95918894b164c47cade40a3dd779d8ce879d
ByteMaster-Purvesh <somwanshipurvesh@gmail.com>
2026-08-17 17:37:07 +0530

    Add State layer on frontend

--- Frontend/src/features/auth/context/auth.Context.js
+++ Frontend/src/features/auth/context/auth.Context.js
@@ -1,32 +1,14 @@
-import axios from 'axios'
+import { Children, createContext } from "react";
 
+export const AuthContext = createContext()
 
-const api = axios.create({
-    baseURL: 'http://localhost:8080',
-    withCredentials : true
-})
+export const AuthProvider = ({Children}) => {
+    const [user, setUser] = useState(null)
+    const [loding, setLoding] = useState(true)
 
-export const registerUserApi = async ({usearname, email, password}) => {
-    const response = await api.post('/api/auth/register', {
-        username, email, password
-    })
-    return response.data
-}   
-
-export const loginUserApi = async ({username, email, password}) => {
-    const responce = await api.post('/api/auth/login', {
-        username, email, password
-    })
-
-    return responce.data
-}
-
-export const getMeUserApi = async () => {
-    const responce = await api.get('/api/auth/get-me')
-    return responce.data 
-}
-
-export const logotUserApi = async () => {
-    const responce = await api.post('/api/auth/logout')
-    return response.data
+    return (
+        <AuthContext.Provider value = {{user, setUser, loding, setLoding}}>
+            {Children}
+        </AuthContext.Provider>
+    )
 }
